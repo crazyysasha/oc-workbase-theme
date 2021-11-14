c27d51472d1434b30012e4be65e8c0f2414155db
ArtemioLangepas <39931236+ArtemioLangepas@users.noreply.github.com>
2021-11-14 14:38:09 +0500

    sadds

--- tailwind.config.js
+++ tailwind.config.js
@@ -100,8 +100,8 @@ module.exports = {
         },
         animation: {
             none: 'none',
-            spin: 'spin 25s linear infinite',
-            spin1: 'spin1 25s linear infinite',
+            spin: 'spin 30s linear infinite',
+            spin1: 'spin1 30s linear infinite',
             ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
             pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
             bounce: 'bounce 1s infinite',
