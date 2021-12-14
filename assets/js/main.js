726ce3a268e496cca62e3d1ae42f5ab4522d5f08
ArtemioLangepas <39931236+ArtemioLangepas@users.noreply.github.com>
2021-12-14 18:51:09 +0500

    verstka

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+$(function() {
+    SmoothScroll({
+        // Время скролла 400 = 0.4 секунды
+        animationTime: 400,
+        // Размер шага в пикселях 
+        stepSize: 100,
+
+        // Дополнительные настройки:
+
+        // Ускорение 
+        accelerationDelta: 30,
+        // Максимальное ускорение
+        accelerationMax: 2,
+
+        // Поддержка клавиатуры
+        keyboardSupport: true,
+        // Шаг скролла стрелками на клавиатуре в пикселях
+        arrowScroll: 50,
+
+        // Pulse (less tweakable)
+        // ratio of "tail" to "acceleration"
+        pulseAlgorithm: true,
+        pulseScale: 4,
+        pulseNormalize: 1,
+
+        // Поддержка тачпада
+        touchpadSupport: true,
+    })
+
+});
