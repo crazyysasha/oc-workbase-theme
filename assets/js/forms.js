c696b439078f2640445b66b7fd18694fe49e1b3b
Sasha Raimov <crazyysasha@gmail.com>
2022-01-12 16:13:48 +0500

    fix

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,36 +1,56 @@
+
 const SearchService = {
     delimiters: ['{', '}'],
-    props: ['category'],
+    props: ['categorySlug'],
     template: '#searchServiceComponent',
+    data() {
+        return {
+            search: '',
+            category: null,
+        };
+    },
+    methods: {
+        async fetchCategory() {
+
+            let res = await (await fetch(`/api/crazy/freelancer/v1.0.1/categories/${this.$props.categorySlug}`)).json();
+            
+            this.category = res;
+        }
+    },
+    created() {
+        this.fetchCategory();
+    },
+    mounted() {
+        console.log('mounted');
+    },
 };
 
 const SelectService = {
     delimiters: ['{', '}'],
-    props: ['category', 'service'],
+    props: ['categorySlug', 'service'],
     template: `#selectServiceComponent`,
 };
+
+
 const routes = [
     {
-        path: '/vue/:category',
+        path: '/vue/:categorySlug',
         component: SearchService,
         props: true,
     },
     {
-        path: '/vue/:category/:service',
+        path: '/vue/:categorySlug/:service+',
         component: SelectService,
         props: true,
     },
 ];
+
 const router = VueRouter.createRouter({
-  history: VueRouter.createWebHistory(),
-  routes,
+    history: VueRouter.createWebHistory(),
+    routes,
 })
 const Form = {
-  delimiters: ['{', '}'],
+    delimiters: ['{', '}'],
 }
 
-const form = Vue.createApp(Form);
-
-form.use(router);
-
-form.mount('#form');
+Vue.createApp(Form).use(router).mount('#form');
